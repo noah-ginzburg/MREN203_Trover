35b5be8b64ed81dbee26e83806f1defe0d4e0888
Noah Ginzburg <noah.ginzburg@gmail.com>
2026-04-01 01:24:56 -0400

    current nav progresS

--- src/trover_communication/trover_communication/trover_serial_comm.py
+++ src/trover_communication/trover_communication/trover_serial_comm.py
@@ -157,7 +157,7 @@ class Trover_Serial_Node(Node):
         # Send over serial
         try:
             self.serial_port.write(data_string.encode('utf-8'))
-            #self.get_logger().info(f'Sent: {data_string.strip()}')
+            self.get_logger().info(f'Sent: {data_string.strip()}')
         except serial.SerialException as e:
             self.get_logger().error(f'Serial write error: {e}')
             self.serial_port = None  # Mark as disconnected
